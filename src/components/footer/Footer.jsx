7230f9e2c80bdf4b1ecf735dc6ccafda50233e13
chandanv09 <chandan.v09@gmail.com>
2024-10-17 19:11:39 +0530

    changes in images

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,10 +7,10 @@ function Footer() {
    <footer className='border-top py-2 mt-5'>
     <Container>
         <Row>
-            <Col md={6}>
+            <Col md={6} col={12} className='d-flex justify-content-md-start justify-content-lg-start justify-content-center'>
             <p className='m-0'>All right reserved copy right © 2024 </p>
             </Col>
-            <Col md={6} className='d-flex justify-content-end'>
+            <Col md={6} className='d-flex justify-content-md-end justify-content-lg-end justify-content-center' col={12}>
             <p className='m-0'>Developed by 
               <a href="https://www.linkedin.com/in/chandan-verma-6663a5a3" target='blank'>  Chandan Verma</a>
               </p>
